06183094e61a5faab6407f5938bd233127543baf
Huayu Qin <445670669@qq.com>
2020-11-04 16:06:37 +0000

    E2E tests - Home page - Filtering by genre

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -59,12 +59,23 @@ describe("Home Page", () => {
                 const matchingMovies = filterByTitle(movies, searchString);
                 cy.get("input").clear().type(searchString);
                 cy.get(".card").should("have.length", 0);
-            
+
             })
         })
         describe("By movie genre", () => {
-            // More later
-        })
+            it("should display movies with the specified genre only", () => {
+                const selectedGenreId = 35;
+                const selectedGenreText = "Comedy";
+                const matchingMovies = filterByGenre(movies, selectedGenreId);
+                cy.get("select").select(selectedGenreText);
+                cy.get(".card").should("have.length", matchingMovies.length);
+                cy.get(".card").each(($card, index) => {
+                    cy.wrap($card)
+                        .find(".card-title")
+                        .should("have.text", matchingMovies[index].title);
+                });
+            });
+        });
 
     })
 })
