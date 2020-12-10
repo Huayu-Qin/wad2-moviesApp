7d9a0a3a94fead918cefe128582196a9d3e19c61
Huayu Qin <445670669@qq.com>
2020-12-10 18:16:49 +0000

    E2E tests - People Details Page

---
+++ cypress/integration/peopleDetails-page.spec.js
@@ -0,0 +1,67 @@
+let peopleId = null
+let people;
+//let reviews;
+describe("People Details Page", () => {
+    before(() => {
+        cy.request(
+            `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+                "TMDB_KEY"
+            )}&language=en-US&page=1`
+        )
+            .its("body")
+            .then((response) => {
+                return response.results[2].id;
+            })
+            .then((arbitraryPeopleIdignored) => {
+                peopleId = arbitraryPeopleIdignored
+                return cy
+                    .request(
+                        `https://api.themoviedb.org/3/person/${peopleId}?api_key=${Cypress.env(
+                            "TMDB_KEY"
+                        )}&language=en-US`
+                    )
+                    .its("body");
+            })
+            .then((peopleDetails) => {
+                people = peopleDetails;
+                return peopleDetails.id;
+            })
+    });
+    beforeEach(() => {
+        cy.visit(`/people/${people.id}`);
+    });
+
+    it("should display peope name in the page header", () => {
+        cy.get("h2").contains(people.name);
+    });
+    it("should display the people's details", () => {
+        cy.get("h4").contains("Biography");
+        // cy.get("h4").next().contains(people.biography);
+        cy.get("ul")
+            .eq(1)
+            .within(() => {
+                cy.get("li").eq(0).contains("Birthday");
+                cy.get("li").eq(1).contains(people.birthday);
+
+            });
+        cy.get("ul")
+            .eq(5)
+            .within(() => {
+                cy.get("li").eq(0).contains("Popularity");
+                cy.get("li").eq(1).contains(people.popularity);
+            });
+    });
+    it("should display the Home icon with the correct URL value", () => {
+        cy.visit(`/people/500`)
+        cy.get(".fa-home")
+          .parent()
+          .should("have.attr", "href")
+        //   .should("include", people.homepage);
+      });
+      it("should display the people poster with the appropriate src attribute", () => {
+        cy.get(".people")
+            .should("have.attr", "src")
+            .should("include", people.profile_path);
+    });
+
+});
