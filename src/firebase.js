8f3bfafcf62561b0711574f7279742b1796d61f3
Huayu Qin <445670669@qq.com>
2020-12-11 13:49:30 +0000

    Fix bugs about firebase API

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,23 @@ import firebase from "firebase"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId:
+    // process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID
+
+    apiKey: "AIzaSyBREn6r2kQivxdBzLC41P90E1BtS0z_3GI",
+    authDomain: "web-development-8d911.firebaseapp.com",
+    databaseURL: "https://web-development-8d911.firebaseio.com",
+    projectId: "web-development-8d911",
+    storageBucket: "web-development-8d911.appspot.com",
     messagingSenderId:
-    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    "22005128573",
+    appId: "1:22005128573:web:24758472b89f608b358014"
 })
 
 export const auth = app.auth()
